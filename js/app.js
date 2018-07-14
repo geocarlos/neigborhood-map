9d8f1f099cc2f5716bad3f992b5bd8e9cb519ef0
Geocarlos Alves <geowildcat@gmail.com>
2018-07-14 15:25:24 -0400

    Add parcel

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
+import '/js/map';
 import ko from '/bower_components/knockout/dist/knockout';
-import ViewModel from '.view_model/view_model';
+import ViewModel from './view_model/view_model';
+import '/css/styles.css';
+import '/bower_components/jquery/dist/jquery.min.js';
+import '/bower_components/bootstrap/dist/js/bootstrap.bundle.min.js';
+import '/bower_components/bootstrap/dist/js/bootstrap.min.js';
+import '/bower_components/bootstrap/dist/css/bootstrap.min.css';
 
 const viewModel = new ViewModel(ko);
 
@@ -7,4 +13,4 @@ function _init() {
   ko.applyBindings(viewModel);
 }
 
-window.onload = _init()
+init()
