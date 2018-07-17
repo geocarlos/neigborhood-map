4bc68a6a2bd6e77e0d95f584b4ba57ed0e08cd4f
Geocarlos Alves <geowildcat@gmail.com>
2018-07-17 10:01:29 -0400

    Add opacity transition to container

--- js/foursquare_api/locations.js
+++ js/foursquare_api/locations.js
@@ -4,5 +4,5 @@ export function getInitialLocations(cb) {
   fetch(`https://api.foursquare.com/v2/venues/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20180716&ll=2.833292,-60.677004&intent=browse&radius=1000`)
   .then(response => response.json())
   .then(data => cb(data))
-  .catch((error) => {window.alert("There was an error fetching locations!"); console.log(error)});
+  .catch((error) => window.alert("There was an error fetching locations!"));
 }
